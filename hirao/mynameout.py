47b569e7d5d7cfabe1539316126f655644211dd2
TakeruisMarvelous <hirao.takeru.hc@mynavi.com>
2024-05-14 04:33:35 +0000

    mynameの回答

--- hirao/mynameout.py
+++ hirao/mynameout.py
@@ -1,6 +1,6 @@
-import sys
-args = sys.argv
+import sys#sysのインポート
+args = sys.argv#argsにsysを設定
 
-name = args[1]
+name = args[1]#引数設定
 
-print("Hello "+name+" !",end = "")
+print("Hello "+name+" !",end = "")#回答文の表示
