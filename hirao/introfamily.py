92c31cbb3643a623c0e1d7389738c4357d614d49
TakeruisMarvelous <hirao.takeru.hc@mynavi.com>
2024-05-20 04:25:03 +0000

    introfamilyの回答No2

--- hirao/introfamily.py
+++ hirao/introfamily.py
@@ -10,12 +10,16 @@ class Intro:
     def age_out(self):
         agetxt = "年齢は、" + str(self.age) + "です"
         return agetxt
+
+class IntroFam(Intro):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+    
     def cat_out(self,cat_name):
-        self.cat_name = cat_name
-        cat_nametxt = "飼い猫の名前は、"+self.cat_name+"です"
+        cat_nametxt = "飼い猫の名前は、"+cat_name+"です"
         return cat_nametxt
 
-Takeru = Intro("健",22)
+Takeru = IntroFam("健",22)
 print(Takeru.name_out())
 print(Takeru.age_out())
 print(Takeru.cat_out("縫"))
